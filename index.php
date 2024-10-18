adcbf07030804f376824adef91b1913c701b9119
realkendo <kennethistifanus1@gmail.com>
2024-10-18 04:10:03 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+  <h1>
+    <?php
+      echo 4 + 5;
+    ?>
+  </h1>
+  <h4>
+    <?php 
+      $message = "my string";
+      echo 'This is ' . $message;
+    ?>
+  </h4>    
+</body>
+</html>
